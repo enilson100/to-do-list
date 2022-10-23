61936e058a0dfe7d8ee4fab9e991f0861c8b2206
enilson silva <enilsonsilva999@gmail.com>
2022-10-23 18:33:17 -0300

    feat:create list todo

--- src/components/Todo/index.tsx
+++ src/components/Todo/index.tsx
@@ -1,5 +1,37 @@
-const Todo = () => {
-  return <div></div>;
+import { useState } from "react";
+import ToDoForm from "../TodoForm";
+import { RiCloseCircleLine } from "react-icons/ri";
+import { IInputData } from "../TodoList";
+import { TiEdit } from "react-icons/ti";
+
+interface IPropsTodo {
+  listTodo: IInputData[];
+  completeListTodo: (id: number) => void;
+}
+
+const Todo = ({ listTodo, completeListTodo }: IPropsTodo) => {
+  const [edit, setEdit] = useState({ id: null, value: "" });
+
+  return (
+    <ul>
+      {listTodo.map((elem, index) => (
+        <li key={index}>
+          <div
+            className={elem.isComplete ? "list_row complete" : "list_row"}
+            key={index}
+          >
+            <div key={elem.id} onClick={() => completeListTodo(elem.id)}>
+              <p>{elem.text}</p>
+            </div>
+            <div className="list_icons">
+              <RiCloseCircleLine />
+              <TiEdit />
+            </div>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default Todo;
--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -1,9 +1,11 @@
 import ToDoForm from "../TodoForm";
+import Todo from "../Todo";
 import { useState } from "react";
 
 export interface IInputData {
   id: number;
   text?: string;
+  isComplete?: boolean;
 }
 
 const TodoList = () => {
@@ -17,10 +19,21 @@ const TodoList = () => {
     setList([todo, ...list]);
   };
 
+  const completeList = (id: number) => {
+    let updatedList = list.map((elem) => {
+      if (elem.id === id) {
+        elem.isComplete = !elem.isComplete;
+      }
+      return elem;
+    });
+    setList(updatedList);
+  };
+
   return (
     <div>
       <h1>What's the Plan for Today?</h1>
       <ToDoForm onSubmit={addTodo} />
+      <Todo listTodo={list} completeListTodo={completeList} />
     </div>
   );
 };
